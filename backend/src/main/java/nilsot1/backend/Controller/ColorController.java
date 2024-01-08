e93a9eb6b5b455a599cafc5c7c2d36fa11e9524d
nils <nilsotto@gmx.de>
2024-01-08 16:16:51 +0100

    add ColorController

---
+++ backend/src/main/java/nilsot1/backend/Controller/ColorController.java
@@ -0,0 +1,12 @@
+package nilsot1.backend.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ColorController {
+
+
+}
