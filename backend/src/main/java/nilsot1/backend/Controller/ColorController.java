4e7d7d5cd59cac64d4895c4095bf83e6ad922d02
nils <nilsotto@gmx.de>
2024-01-08 16:21:05 +0100

    add PostMapping getColors

--- backend/src/main/java/nilsot1/backend/Controller/ColorController.java
+++ backend/src/main/java/nilsot1/backend/Controller/ColorController.java
@@ -1,12 +1,26 @@
 package nilsot1.backend.Controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/api")
 public class ColorController {
 
+    @PostMapping("/getColors")
+    public String getColors(@RequestBody String requestBody) {
 
+        WebClient webclient = WebClient.create();
+
+        return webclient.post()
+                .uri("http://colormind.io/api/")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 }
