fe4f1eafd3c17a25a1ec227611233a01c54f40eb
nils <nilsotto@gmx.de>
2024-01-10 15:20:57 +0100

    refactor repo

--- backend/src/main/java/nilsot1/backend/service/ColorRoomSetService.java
+++ backend/src/main/java/nilsot1/backend/service/ColorRoomSetService.java
@@ -1,14 +1,14 @@
 package nilsot1.backend.service;
 
 import lombok.RequiredArgsConstructor;
-import nilsot1.backend.repository.ColorRoomSetRepo;
+import nilsot1.backend.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ColorRoomSetService {
 
-    private final ColorRoomSetRepo repo;
+    private final UserRepo repo;
 
 
 }
