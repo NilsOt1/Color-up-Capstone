da87805e9942b97ff14de422680685678099ddc5
nils <nilsotto@gmx.de>
2024-01-10 10:18:33 +0100

    add ColorRoomSetController

---
+++ backend/src/main/java/nilsot1/backend/controller/ColorRoomSetController.java
@@ -0,0 +1,12 @@
+package nilsot1.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ColorRoomSetController {
+
+}
