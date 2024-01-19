d6861260e702380d98393e92c62b3fa8fff3b7fb
nils <nilsotto@gmx.de>
2024-01-19 11:40:25 +0100

    refactor test

--- backend/src/test/java/nilsot1/backend/controller/UserControllerTest.java
+++ backend/src/test/java/nilsot1/backend/controller/UserControllerTest.java
@@ -1,12 +1,15 @@
 package nilsot1.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nilsot1.backend.model.User;
+import nilsot1.backend.model.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -22,38 +25,26 @@ class UserControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void testGetAllColorRoomSets_shouldReturnEmptyList_whenCalledInitially() throws Exception {
-        User user = new User("1", "test", new ArrayList<>());
+        UserDto user = new UserDto("test", new ArrayList<>());
+        String newUserAsJSON = objectMapper.writeValueAsString(user);
 
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                                {
-                                "userId": "1",
-                                "userName": "test",
-                                "colorRoomSets": []
-                                }
-                                """))
-
+                        .content(newUserAsJSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("""
-                        {
-                        "userId": "1",
-                        "userName": "test",
-                        "colorRoomSets": []
-                        }
-                        """));
+                .andExpect(MockMvcResultMatchers.content().json(newUserAsJSON))
+                        .andReturn();
 
+        User savedUser = objectMapper.readValue(result.getResponse().getContentAsString(), User.class);
+        String userAsJSON = objectMapper.writeValueAsString(savedUser);
 
-        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + user.getUserId()))
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + savedUser.getUserId()))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json("""
-                        {
-                        "userId": "1",
-                        "userName": "test",
-                        "colorRoomSets": []
-                        }
-                        """));
+                .andExpect(MockMvcResultMatchers.content().json(userAsJSON));
     }
 }
