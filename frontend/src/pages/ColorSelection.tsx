808bc7040d8565c861c6c9f5c428353df6621fb3
nils <nilsotto@gmx.de>
2024-01-25 20:36:55 +0100

    fetch Data for colorRoomSet on ColorSelection page

--- frontend/src/pages/ColorSelection.tsx
+++ frontend/src/pages/ColorSelection.tsx
@@ -1,8 +1,36 @@
-import Colors from "../components/Colors.tsx";
+import {useState} from "react";
+import axios from "axios";
+import {Room} from "../types/Room.ts";
+import {ColorPalette} from "../types/ColorPalette.ts";
+import {useParams} from "react-router";
+import DisplayedColors from "../components/DisplayedColors.tsx";
 
 export default function ColorSelection() {
+
+    const [colorRoomSet, setColorRoomSet] = useState<{
+        colorRoomSetId: string,
+        room: Room,
+        savedColors: ColorPalette | []
+    }>({ colorRoomSetId: "", room: { roomId: "", roomName: "" }, savedColors: [] })
+
+    const {colorRoomSetId} = useParams()
+
+
+
+    function getColorRoomSetById() {
+        axios
+            .get("/api/user/cf0ff01b-8d19-4211-9a0b-6eb0aeec165e/color-room-sets/" + colorRoomSetId)
+            .then(response => {
+                setColorRoomSet(response.data)
+            })
+
+            .catch(error => {
+                console.error("Error", error)
+            })
+    }
+
     return (
-        <Colors/>
+        <DisplayedColors colorRoomSet={colorRoomSet} getColorRoomSetById={getColorRoomSetById}/>
     )
 
 }
